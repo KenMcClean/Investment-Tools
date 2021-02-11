cf7a9929a1bb925aadaf82d9f39a1118c42cf8ab
KenMcClean <65670906+KenMcClean@users.noreply.github.com>
2021-02-11 11:41:02 -0800

    Update and rename PS-INVEST-TSX-LISTING-SCRAPE.ps1 to PS-INVEST-TSX-TSXV-LISTING-SCRAPE.ps1

--- PS-INVEST-TSX-TSXV-LISTING-SCRAPE.ps1
+++ PS-INVEST-TSX-TSXV-LISTING-SCRAPE.ps1
@@ -1,7 +1,18 @@
-#The TSE exposes it's listings as JSON
 $pages = @('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '0-9')
-
+write-host "Exchange: TSX"
 foreach($page in $pages){
 $data = Invoke-RestMethod -uri "https://www.tsx.com/json/company-directory/search/tsx/$page"
 $data.results| Select-Object symbol,name
+
 }
+
+
+
+
+write-host "Exchange: TSX-V"
+foreach($page in $pages){
+$data = Invoke-RestMethod -uri "https://www.tsx.com/json/company-directory/search/tsxv/$page"
+$data.results| Select-Object symbol,name
+
+}
+
